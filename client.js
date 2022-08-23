c24f6868eca3777425f3cce5707d2e95a62aa0c6
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-23 16:31:59 -0500

    create bonus calculator

--- client.js
+++ client.js
@@ -31,6 +31,64 @@ const employees = [
   }
 ];
 
+for(let employee of employees){
+  console.log(bonusCalc(employee));
+}
+
+function bonusCalc(employee){
+  bonusParameter1(employee);
+  bonusParameter2(employee);
+  bonusParameter3(employee);
+  bonusParameter4();
+  let employeeBonusStuff = {
+    employeeName: employee.name,
+    bonusPercentage: bonusPercentage*100,
+    totalBonus: bonusPercentage * employee.annualSalary,
+    totalCompensation: Number(bonusPercentage * employee.annualSalary) + Number(employee.annualSalary)
+  }
+  return employeeBonusStuff;
+}
+
+function bonusParameter1(employee){
+  if(employee.reviewRating <= 2){
+    bonusPercentage = 0;
+  }
+  else if(employee.reviewRating === 3){
+    bonusPercentage = .04;
+  }
+  else if(employee.reviewRating === 4){
+    bonusPercentage = .06;
+  }
+  else if(employee.reviewRating === 5){
+  bonusPercentage = .1;
+  }
+  return bonusPercentage;
+}
+
+function bonusParameter2(employee){
+  if(employee.employeeNumber.length === 4){
+    bonusPercentage += .05;
+  }
+  return bonusPercentage;
+}
+
+function bonusParameter3(employee){
+  if(employee.annualSalary > '65000'){
+    bonusPercentage -= .01;
+  }
+  return bonusPercentage;
+}
+
+function bonusParameter4(){
+  if(bonusPercentage > .13){
+    bonusPercentage = .13;
+  }
+  else if(bonusPercentage < 0){
+    bonusPercentage = 0;
+  }
+  return bonusPercentage;
+}
+
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // This problem is massive! Break the problem down, take small steps, and test as you go.
@@ -39,4 +97,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+//console.log( employees );
